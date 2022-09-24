9da0930eeec4a309098dac250845d1ae6f22ca4c
Richard Stock <rstock.co@gmail.com>
2022-09-24 12:52:14 -0600

    Edits to schema

--- db/schema/05_menu_categories_dishes.sql
+++
@@ -1,9 +0,0 @@
--- Drop and recreate Menu Categories - Dishes table
-
-DROP TABLE IF EXISTS menu_categories_dishes CASCADE;
-
-CREATE TABLE menu_categories_dishes (
-  id SERIAL PRIMARY KEY NOT NULL,
-  menu_category_id INTEGER NOT NULL REFERENCES menu_categories(id) ON DELETE CASCADE,
-  dish_id INTEGER NOT NULL REFERENCES dishes(id) ON DELETE CASCADE
-);
