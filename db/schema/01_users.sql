aff82bd3c5d159268e22b95dac550d7019ae5f50
Richard Stock <rstock.co@gmail.com>
2022-09-25 13:31:55 -0600

    Fix bug in users db schema

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -8,5 +8,5 @@ CREATE TABLE users (
   last_name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  phone_number VARCHAR(15) NOT NULL,
+  phone_number VARCHAR(15) NOT NULL
 );
