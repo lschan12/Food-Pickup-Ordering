e700ca8bb97e655666f8b0c02dea46f646beacda
Richard Stock <rstock.co@gmail.com>
2022-09-25 12:51:10 -0600

    Fix bug for initial loading of dishes and refactor

--- public/scripts/dishes.js
+++ public/scripts/dishes.js
@@ -1,29 +1,28 @@
 $(document).ready(function () {
   console.log("running load dishes");
-  loadDishes();
+  loadDishes("All");
 });
 
 const createDishElement = (obj) => {
-  const element = $(`
-  <article>
-  <div class="dish-detail">
-  <label>${obj.name}</label>
-  <div>${obj.description}</div>
-  <div>$${obj.price / 100}</div>
-  <button id="add-to-cart">Add to Cart</button>
-  </div>
-  <img src="${obj.photo_url}">
-  </article>
-  `);
+  const element = 
+  `<article>
+    <div class="dish-detail">
+      <label>${obj.name}</label>
+      <div>${obj.description}</div>
+      <div>$${obj.price / 100}</div>
+      <button id="add-to-cart">Add to Cart</button>
+    </div>
+    <img src="${obj.photo_url}">
+  </article>`;
   return element;
 };
 
 const renderDishes = (dishes, category) => {
   let filteredDishes = dishes;
   if (category !== null && category !== "All") {
-    filteredDishes = dishes.filter(dish => dish.category === category);
+    filteredDishes = dishes.filter((dish) => dish.category === category);
   }
-  filteredDishes.forEach(dish => {
+  filteredDishes.forEach((dish) => {
     const generatedDish = createDishElement(dish);
     $("#dishes-container").append(generatedDish);
   });
